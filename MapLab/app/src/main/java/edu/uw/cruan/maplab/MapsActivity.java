e0e29b3a6bcf273fd61ac412bdadf79bc9dfd4f6
Cherie Ruan <cherie_ruan@hotmail.com>
2017-11-06 19:28:45 -0800

    Finish lab

---
+++ MapLab/app/src/main/java/edu/uw/cruan/maplab/MapsActivity.java
@@ -0,0 +1,77 @@
+package edu.uw.cruan.maplab;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        //LatLng sydney = new LatLng(-34, 151);
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        final LatLng FOUNTAIN = new LatLng(47.653802, -122.307803);
+        mMap.addMarker(new MarkerOptions()
+            .position(FOUNTAIN)
+            .title("Drumheller Fountain")
+            .snippet("In 2017, a new family of ducks were born!"))
+            .setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET));
+
+        /*LatLng start1 = new LatLng(46, -120);
+        LatLng fin1 = new LatLng(50, -122.307803);
+        LatLng fin2 = new LatLng(47.6, -122.2888);
+        PolylineOptions lines = new PolylineOptions();
+        lines.color(Color.WHITE);
+        lines.width(200);
+        lines.add(start1);
+        lines.add(fin1);
+        lines.add(fin2);*/
+
+        Polyline line = mMap.addPolyline(new PolylineOptions()
+            .add(new LatLng(47.653, -122.3078025), new LatLng(47.653802, -122.307803))
+            .add(new LatLng(47.653, -122.3078025), new LatLng(47.653802, -122.3077))
+                .add(new LatLng(47.653802, -122.3077), new LatLng(47.653, -122.3076))
+                .add(new LatLng(47.653, -122.3), new LatLng(47.653802, -122.30))
+            .width(5)
+            .color(Color.WHITE)
+            .geodesic(true)
+            .visible(true));
+
+    }
+}
